8efc398f0048ed5ec28fc5d60f65452271daa4f0
Mike <mfischetti2192@gmail.com>
2016-09-18 15:46:50 -0400

    added body-parser

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ require('dotenv').config()
 var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
+var bodyParser= require('body-parser')
 var PORT = process.env.PORT || 3000;
 var database = require('./config/database');
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 //connect to remote mongodb
 mongoose.connect(database.url); 
@@ -12,9 +15,9 @@ mongoose.connect(database.url);
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 }); 
-app.get('/idea', function(req, res){
-  console.log('New idea posted');
-});
+app.post('/idea', function(req, res){
+  console.log('New idea posted: '+req.body.idea+'\n'+'Description: '+req.body.description);
+})
 
 app.listen(PORT, function(){
   console.log('listening on port: '+ PORT);
